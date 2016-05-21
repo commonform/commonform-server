c49b559998c4601e5a08109d0565e92fbeecd215
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-20 22:58:02 -0700

    Test top-level 404 response

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function handleHTTPRequest(bole, level) {
       eventLog.info({ event: 'emit', name: event }) })
     // A new form has been added to the library.
     .on('form', function emitEventsForChildren(form, digest, normalized, seen) {
+      /* istanbul ignore if */
       if (seen.includes(digest)) {
         eventLog.error({ event: 'collision', digest: digest, seen: seen }) }
       else {
