baac8abea9ad45982a1f614c55a0ad944b084be6
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-20 22:58:02 -0700

    Test top-level 404 response

---
+++ test/not-found.test.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var server = require('./server')
+var tape = require('tape')
+
+tape('GET /nothing_here', function(test) {
+  server(function(port, done) {
+    var request = { path: '/noting_here', port: port }
+    http.get(request, function(response) {
+      test.equal(response.statusCode, 404, 'responds 404')
+      done() ; test.end() }) }) })
