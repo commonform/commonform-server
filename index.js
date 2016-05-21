9fb1cda42b4d5f7eb4003fca59f5ad801845fc29
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-20 17:10:26 -0700

    POST and GET forms

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 module.exports = handler
 
+var EventEmitter = require('eventemitter2').EventEmitter2
+var concat = require('concat-stream')
+var encode = require('bytewise/encoding/hex').encode
+var isDigest = require('is-sha-256-hex-digest')
+var normalize = require('commonform-normalize')
+var parse = require('json-parse-errback')
 var url = require('url')
 var uuid = require('uuid')
+var validForm = require('commonform-validate').form
 
-var metadata = (function() {
-  var meta = require('./package.json')
-  return JSON.stringify(
-    { service: meta.name,
-      version: meta.version }) })()
+var VERSION = require('./package.json').version
+var METADATA = JSON.stringify(
+  { service: require('./package.json').name,
+    version: VERSION })
 
-function handler(bole) {
+function handler(bole, level) {
+  var eventLog = bole('events')
+  var eventBus = new EventEmitter
+  eventBus.onAny(function(event) {
+    eventLog.info({ event: 'emit', name: event }) })
+
   return function(request, response) {
     request.log = bole(uuid.v4())
     request.log.info(request)
@@ -19,10 +30,67 @@ function handler(bole) {
     if (pathname === '/') {
       if (method === 'GET') {
         response.setHeader('content-type', 'application/json')
-        response.end(metadata) }
+        response.end(METADATA) }
       else {
         response.statusCode = 405
         response.end() } }
+    if (pathname === '/forms') {
+      if (method === 'POST') {
+        request.pipe(concat(function(buffer) {
+          parse(buffer, function(error, form) {
+            if (error) {
+              request.log.info({ event: 'invalid JSON' })
+              response.statusCode = 400
+              response.end('invalid JSON') }
+            else {
+              if (!validForm(form)) {
+                request.log.info({ event: 'invalid form' })
+                response.statusCode = 400
+                response.end('invalid form') }
+              else {
+                var root = normalize(form).root
+                request.log.info({ digest: root })
+                var key = encode([ 'forms', root ])
+                request.log({ key: key })
+                var value = JSON.stringify({ version: VERSION, form: form })
+                level.put(key, value, function(error) {
+                  /* istanbul ignore if */
+                  if (error) {
+                    request.log.error(error)
+                    response.statusCode = 500
+                    response.end('database error') }
+                  else {
+                    request.log.info({ event: 'form' })
+                    response.statusCode = 201
+                    response.setHeader('location', ( '/forms/' + root ))
+                    response.end()
+                    eventBus.emit('form', form) } }) } } }) })) }
+      else {
+        response.statusCode = 405
+        response.end() } }
+    else if (pathname.startsWith('/forms/')) {
+      var digest = pathname.substring('/forms/'.length)
+      if (!isDigest(digest)) {
+        request.log.info({ event: 'invalid digest' })
+        response.statusCode = 404
+        response.end() }
+      else {
+        var key = encode([ 'forms', digest ])
+        level.get(key, function(error, value) {
+          /* istanbul ignore if */
+          if (error) {
+            if (error.notFound) {
+              request.log.info({ event: 'not found' })
+              response.statusCode = 404
+              response.end() }
+            else {
+              request.log.error(error)
+              response.statusCode = 500
+              response.end('database error') } }
+          else {
+            var form = JSON.parse(value).form
+            response.setHeader('content-type', 'application/json')
+            response.end(JSON.stringify(form)) } }) } }
     else {
       response.statusCode = 404
       response.end() } } }
---
+++ test/forms.test.js
@@ -0,0 +1,87 @@
+var concat = require('concat-stream')
+var http = require('http')
+var normalize = require('commonform-normalize')
+var server = require('./server')
+var tape = require('tape')
+
+tape('POST /forms with invalid JSON', function(test) {
+  server(function(port, done) {
+    var request = { method: 'POST', path: '/forms', port: port }
+    http
+      .request(request, function(response) {
+        test.equal(response.statusCode, 400, 'responds 400')
+        response.pipe(concat(function(buffer) {
+          test.equal(
+            buffer.toString(), 'invalid JSON',
+            'responds "invalid JSON"')
+          done() ; test.end() })) })
+      .end('not valid json') }) })
+
+tape('POST /forms with form', function(test) {
+  server(function(port, done) {
+    var form = { content: [ 'Just a test' ] }
+    var root = normalize(form).root
+    var request = { method: 'POST', path: '/forms', port: port }
+    http
+      .request(request, function(response) {
+        test.equal(response.statusCode, 201, 'responds 200')
+        test.equal(
+          response.headers.location, ( '/forms/' + root ),
+          'sets location header')
+          done() ; test.end() })
+      .end(JSON.stringify(form)) }) })
+
+tape('POST /forms with form', function(test) {
+  server(function(port, done) {
+    var form = { invalid: 'form' }
+    var request = { method: 'POST', path: '/forms', port: port }
+    http
+      .request(request, function(response) {
+        test.equal(response.statusCode, 400, 'responds 400')
+        response.pipe(concat(function(buffer) {
+          test.equal(
+            buffer.toString(), 'invalid form',
+            'serves "invalid form"')
+          done() ; test.end() })) })
+      .end(JSON.stringify(form)) }) })
+
+tape('GET /forms/$not_a_digest', function(test) {
+  server(function(port, done) {
+    var digest = 'blah'
+    var request = { path: ( '/forms/' + digest ), port: port }
+    http.get(request, function(response) {
+      test.equal(response.statusCode, 404, 'responds 404')
+      done() ; test.end() }) }) })
+
+tape('GET /forms/$nonexistent', function(test) {
+  server(function(port, done) {
+    var digest = 'a'.repeat(64)
+    var request = { path: ( '/forms/' + digest ), port: port }
+    http.get(request, function(response) {
+      test.equal(response.statusCode, 404, 'responds 404')
+      done() ; test.end() }) }) })
+
+tape('GET /forms/$posted', function(test) {
+  server(function(port, done) {
+    var form = { content: [ 'Test form' ] }
+    var root = normalize(form).root
+    var post = { method: 'POST', path: '/forms', port: port }
+    http
+      .request(post, function(response) {
+        test.equal(response.statusCode, 201, 'responds 201')
+        var get = { path: ( '/forms/' + root ), port: port }
+        http.get(get, function(response) {
+          response.pipe(concat(function(buffer) {
+            test.same(JSON.parse(buffer), form, 'serves the posted form')
+            done() ; test.end() })) }) })
+      .end(JSON.stringify(form)) }) })
+
+tape('PUT /forms', function(test) {
+  server(function(port, done) {
+    var request = { method: 'PUT', path: '/forms', port: port }
+    http
+      .request(request, function(response) {
+        test.equal(response.statusCode, 405, 'responds 405')
+        done() ; test.end() })
+      .end() }) })
+
