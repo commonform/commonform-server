a76832e01d3abca3aa8a493e135f43aa56fdc9fc
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-22 15:44:16 -0700

    Pull out /forms route

---
+++ routes/forms.js
@@ -0,0 +1,31 @@
+module.exports = forms
+
+var badRequest = require('./bad-request')
+var internalError = require('./internal-error')
+var methodNotAllowed = require('./method-not-allowed')
+var normalize = require('commonform-normalize')
+var putForm = require('../queries/put-form')
+var readJSONBody = require('./read-json-body')
+var validForm = require('commonform-validate').form
+
+function forms(emit, request, response, parameters, log, level) {
+  if (request.method === 'POST') {
+    readJSONBody(request, response, function(form) {
+      if (!validForm(form)) { badRequest(response, 'invalid form') }
+      else {
+        var normalized = normalize(form)
+        var digest = normalized.root
+        response.log.info({ digest: digest })
+        putForm(level, digest, form, true, function(error) {
+          /* istanbul ignore if */
+          if (error) { internalError(response, error) }
+          else {
+            response.log.info({ event: 'form' })
+            response.statusCode = 201
+            response.setHeader('Location', ( '/forms/' + digest ))
+            response.end()
+            // Emit an event for the new form. This will trigger
+            // indexing and other processing by event handlers
+            // on the event emitter.
+            emit('form', form, digest, normalized, [ ]) } }) } }) }
+  else { methodNotAllowed(response) } }
---
+++ routes/read-json-body.js
@@ -0,0 +1,59 @@
+module.exports = readJSONBody
+
+var badRequest = require('./bad-request')
+var parseJSON = require('json-parse-errback')
+var internalError = require('./internal-error')
+var requestEntityTooLarge = require('./request-entity-too-large')
+
+var LIMIT = ( parseInt(process.env.MAX_BODY_SIZE) || 256 )
+
+function readJSONBody(request, response, callback) {
+  var buffer
+  var finished = false
+  var bytesReceived = 0
+  var lengthHeader = request.headers['content-length']
+  var tooLarge = ( lengthHeader && ( parseInt(lengthHeader) > LIMIT ) )
+  if (tooLarge) {
+    requestEntityTooLarge(response) }
+  else {
+    buffer = [ ]
+    request.on('data', onData)
+    request.once('abort', onAbort)
+    request.once('close', finish)
+    request.once('end', onEnd)
+    request.once('error', onEnd) }
+  function onData(chunk) {
+    if (!finished) {
+      buffer.push(chunk)
+      bytesReceived += chunk.length
+      if (bytesReceived > LIMIT) {
+        finish()
+        requestEntityTooLarge(response) } } }
+  function onAbort() {
+    if (!finished) {
+      finish()
+      badRequest(response, 'request aborted') } }
+  function onEnd(error) {
+    if (!finished) {
+      if (error) {
+        request.pause()
+        finish()
+        internalError(response, error) }
+      else {
+        var inaccurateHeader = (
+          lengthHeader && ( parseInt(lengthHeader) !== bytesReceived ) )
+        if (inaccurateHeader) {
+          finish()
+          badRequest(response, 'inaccurate Content-Length') }
+        else {
+          parseJSON(Buffer.concat(buffer), function(error, object) {
+            finish()
+            if (error) { badRequest(response, 'invalid JSON') }
+            else { callback(object) } }) } } } }
+  function finish() {
+    if (!finished) {
+      finished = true
+      buffer = null
+      request.removeAllListeners()
+      request.pause() } } }
+
