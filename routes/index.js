f7390ac08a541708e9889a999725a0e5f8c8feb1
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-22 16:05:59 -0700

    Refactor router

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,11 @@
 var routes = module.exports = require('http-hash')()
 
-var metadata = require('./metadata')
-var forms = require('./forms')
-var formsDigest = require('./forms-digest')
-
-routes.set('/', metadata)
-
-routes.set('/forms', forms)
-
-routes.set('/forms/:digest', formsDigest)
-
-routes.set('/publishers/:publisher/projects/:project/editions/:edition', require('./edition'))
-
-routes.set('/publishers/:publisher/projects/:project/editions', require('./project-editions'))
-
-routes.set('/publishers/:publisher/projects/:project/editions/:edition/form', require('./edition-form'))
-
-routes.set('/publishers', require('./publishers'))
-
-routes.set('/publishers/:publisher/projects', require('./publisher-projects'))
-
+routes.set('/', require('./metadata'))
+routes.set('/forms', require('./forms'))
+routes.set('/forms/:digest', require('./form'))
 routes.set('/forms/:digest/projects', require('./form-projects'))
+routes.set('/publishers', require('./publishers'))
+routes.set('/publishers/:publisher/projects', require('./publisher-projects'))
+routes.set('/publishers/:publisher/projects/:project/editions', require('./project-editions'))
+routes.set('/publishers/:publisher/projects/:project/editions/:edition', require('./edition'))
+routes.set('/publishers/:publisher/projects/:project/editions/:edition/form', require('./edition-form'))
