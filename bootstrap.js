82b9d5924390c70eab6423978ad57c2991c1b8dd
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-24 00:07:13 -0700

    Bootstrap forms before projects

--- bootstrap.js
+++ bootstrap.js
@@ -5,19 +5,18 @@ if (!url) {
   process.stderr.write('Usage: <URL>\n')
   process.exit(1) }
 
+var server = require('url').parse(url)
+
 if (!process.env.hasOwnProperty('ADMINISTRATOR_PASSWORD')) {
   process.stderr.write('Set ADMINISTRATOR_PASSWORD')
   process.exit(1) }
 
 var password = process.env.ADMINISTRATOR_PASSWORD
 
-var server = require('url').parse(url)
-
+var each = require('async-each')
 var http = require('http')
-var pino = require('pino')
-
+var log = require('pino')()
 var s3 = require('./s3')
-var log = pino()
 
 log.info({ event: 'starting' })
 
@@ -25,35 +24,41 @@ if (!s3) {
   log.error({ event: 'error', message: 'no s3'})
   process.exit(1) }
 
-var formLog = log.child({ type: 'form' })
-bootstrapForms()
+var formsLog = log.child({ type: 'forms' })
+eachObject('forms/', formsLog, postForm, false, function() {
+  formsLog.info({ event: 'done' })
+  var projectsLog = log.child({ type: 'projects' })
+  eachObject('projects/', projectsLog, postProject, false, function() {
+    projectsLog.info({ event: 'done' })
+    log.info({ event: 'done' }) }) })
 
-var projectsLog = log.child({ type: 'project' })
-bootstrapProjects()
-
-function bootstrapForms(marker) {
-  var query = { Prefix: 'forms/' }
+function eachObject(prefix, log, iterator, marker, callback) {
+  var query = { Prefix: prefix }
   if (marker) { query.Marker = marker }
-  formLog.info({ event: 'querying' })
+  log.info({ event: 'querying' })
   s3.listObjects(query, function(error, data) {
-    if (error) {
-      log.error({ event: 'query error' }, error) }
+    if (error) { log.error({ event: 'query error' }, error) }
     else {
       var keys = data.Contents
         .map(function(element) { return element.Key })
-        .filter(function(key) { return ( key !== 'forms/' ) })
-      keys.forEach(function(key) {
-        var keyLog = formLog.child({ key: key })
-        keyLog.info({ event: 'get', key: key })
-        s3.getObject({ Key: key }, function(error, data) {
-          if (error) {
-            keyLog.error({ event: 'get error' }, error) }
-          else {
-            postForm(JSON.parse(data.Body).form, keyLog) } }) })
-      if (data.IsTruncated) {
-        bootstrapForms(keys[keys.length - 1]) } } }) }
+        .filter(function(key) { return !key.endsWith('/') })
+      each(
+        keys,
+        function(key, done) {
+          var keyLog = log.child({ key: key })
+          keyLog.info({ event: 'get', key: key })
+          s3.getObject({ Key: key }, function(error, data) {
+            if (error) {
+              keyLog.error({ event: 'get error' }, error)
+              done() }
+            else { iterator(JSON.parse(data.Body), keyLog, done) } }) },
+        function() {
+          if (data.IsTruncated) {
+            var lastKey = keys[keys.length - 1]
+            eachObject(prefix, log, iterator, lastKey) }
+          else { callback() } }) } }) }
 
-function postForm(form, log) {
+function postForm(record, log, callback) {
   var request =
     { protocol: server.protocol,
       host: server.hostname,
@@ -64,35 +69,12 @@ function postForm(form, log) {
   http
     .request(request, function(response) {
       var status = response.statusCode
-      if(status === 201) {
-        log.info({ event: 'wrote' }) }
-      else {
-        log.error({ event: 'write error', status: status }) } })
-    .end(JSON.stringify(form)) }
+      if (status === 201) { log.info({ event: 'wrote' }) }
+      else { log.error({ event: 'write error', status: status }) }
+      callback() })
+    .end(JSON.stringify(record.form)) }
 
-function bootstrapProjects(marker) {
-  var query = { Prefix: 'projects/' }
-  if (marker) { query.Marker = marker }
-  projectsLog.info({ event: 'querying' })
-  s3.listObjects(query, function(error, data) {
-    if (error) {
-      log.error({ event: 'query error' }, error) }
-    else {
-      var keys = data.Contents
-        .map(function(element) { return element.Key })
-        .filter(function(key) { return !key.endsWith('/') })
-      keys.forEach(function(key) {
-        var keyLog = projectsLog.child({ key: key })
-        keyLog.info({ event: 'get', key: key })
-        s3.getObject({ Key: key }, function(error, data) {
-          if (error) {
-            keyLog.error({ event: 'get error' }, error) }
-          else {
-            postProject(JSON.parse(data.Body), keyLog) } }) })
-      if (data.IsTruncated) {
-        bootstrapForms(keys[keys.length - 1]) } } }) }
-
-function postProject(record, log) {
+function postProject(record, log, callback) {
   var request =
     { protocol: server.protocol,
       host: server.hostname,
@@ -107,8 +89,7 @@ function postProject(record, log) {
   http
     .request(request, function(response) {
       var status = response.statusCode
-      if(status === 201) {
-        log.info({ event: 'wrote' }) }
-      else {
-        log.error({ event: 'write error', status: status }) } })
+      if (status === 201) { log.info({ event: 'wrote' }) }
+      else { log.error({ event: 'write error', status: status }) }
+      callback() })
     .end(JSON.stringify({ digest: record.digest })) }
