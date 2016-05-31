2869727ab10eb5adda7b8d50113a2a06c56c2350
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-30 17:46:26 -0700

    Remove redundant request method check

--- routes/annotations.js
+++ routes/annotations.js
@@ -98,57 +98,55 @@ function computeContexts(normalized) {
     return result } }
 
 function postAnnotation(request, response, parameters, log, level, emit) {
-  if (request.method === 'POST') {
-    readJSONBody(request, response, function(annotation) {
-      var put = function() {
-        putAnnotation(level, annotation, function(error) {
-          if (error) { internalError(response) }
-          else {
-            response.log.info({ event: 'annotation' })
-            response.statusCode = 201
-            var location = ( '/annotations/' + annotation.uuid )
-            response.setHeader('Location', location)
-            response.end()
-            emit('annotation', annotation) } }) }
-      var authorized =
-        ( ( request.publisher === 'administrator' ) ||
-          ( request.publisher === annotation.publisher ) )
-      if (!validAnnotation(annotation)) {
-        badRequest(response, 'invalid annotation') }
-      else if (!authorized ) {
-        if (request.publisher === undefined) { unauthorized(response) }
-        else { forbidden(response) } }
-      else {
-        if (request.publisher === 'administrator') { put() }
+  readJSONBody(request, response, function(annotation) {
+    var put = function() {
+      putAnnotation(level, annotation, function(error) {
+        if (error) { internalError(response) }
         else {
-          annotation.publisher = parameters.publisher
-          annotation.uuid = uuid.v4()
-          response.log.info({ event: 'uuid', uuid: annotation.uuid })
-          annotation.timestamp = Date.now().toString()
-          // Does the server have the context form?
-          getForm(level, annotation.context, function(error, context) {
-            if (error) {
-              if (error.notFound) { badRequest(response, 'Unknown context') }
-              else { internalError(response, error) } }
+          response.log.info({ event: 'annotation' })
+          response.statusCode = 201
+          var location = ( '/annotations/' + annotation.uuid )
+          response.setHeader('Location', location)
+          response.end()
+          emit('annotation', annotation) } }) }
+    var authorized =
+      ( ( request.publisher === 'administrator' ) ||
+        ( request.publisher === annotation.publisher ) )
+    if (!validAnnotation(annotation)) {
+      badRequest(response, 'invalid annotation') }
+    else if (!authorized ) {
+      if (request.publisher === undefined) { unauthorized(response) }
+      else { forbidden(response) } }
+    else {
+      if (request.publisher === 'administrator') { put() }
+      else {
+        annotation.publisher = parameters.publisher
+        annotation.uuid = uuid.v4()
+        response.log.info({ event: 'uuid', uuid: annotation.uuid })
+        annotation.timestamp = Date.now().toString()
+        // Does the server have the context form?
+        getForm(level, annotation.context, function(error, context) {
+          if (error) {
+            if (error.notFound) { badRequest(response, 'Unknown context') }
+            else { internalError(response, error) } }
+          else {
+            // Is the annotated form within the context?
+            context = JSON.parse(context)
+            var childrenDigests = Object.keys(normalize(context.form))
+            if (childrenDigests.indexOf(annotation.form) === -1) {
+              badRequest(response, 'Form not in context') }
             else {
-              // Is the annotated form within the context?
-              context = JSON.parse(context)
-              var childrenDigests = Object.keys(normalize(context.form))
-              if (childrenDigests.indexOf(annotation.form) === -1) {
-                badRequest(response, 'Form not in context') }
-              else {
-                if (annotation.replyTo) {
-                  getAnnotation(level, annotation.replyTo, function(error, stored) {
-                    var prior = JSON.parse(stored).annotation
-                    if (error) {
-                      if (error.notFound) { badRequest(response, 'Invalid replyTo') }
-                      else { internalError(response, error) } }
-                    else {
-                      var sameTarget = (
-                        ( annotation.context === prior.context ) &&
-                        ( annotation.form === prior.form ) )
-                      if (!sameTarget) {
-                        badRequest(response, 'Does not match replyTo') }
-                      else { put() } } }) }
-                else { put() } } } }) } } }) }
-  else { methodNotAllowed(response) } }
+              if (annotation.replyTo) {
+                getAnnotation(level, annotation.replyTo, function(error, stored) {
+                  var prior = JSON.parse(stored).annotation
+                  if (error) {
+                    if (error.notFound) { badRequest(response, 'Invalid replyTo') }
+                    else { internalError(response, error) } }
+                  else {
+                    var sameTarget = (
+                      ( annotation.context === prior.context ) &&
+                      ( annotation.form === prior.form ) )
+                    if (!sameTarget) {
+                      badRequest(response, 'Does not match replyTo') }
+                    else { put() } } }) }
+              else { put() } } } }) } } }) }
