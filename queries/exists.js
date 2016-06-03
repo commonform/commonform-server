6aa259422fb0b73323fd4da0b6343cd483c6ebea
Kyle E. Mitchell <kyle@kemitchell.com>
2016-06-03 00:52:57 -0700

    Retry existence queries

--- queries/exists.js
+++ queries/exists.js
@@ -1,8 +1,12 @@
-// TODO retry existence checks
+var isNotFoundError = require('../is-not-found-error')
+var thrice = require('../thrice')
+
 module.exports = function(level, key, callback) {
-  level.get(key, function(error) {
+  var get = level.get.bind(level, key)
+  thrice(get, onResult, isNotFoundError)
+  function onResult(error) {
     if (error) {
       /* istanbul ignore else */
       if (error.notFound) { callback(null, false) }
       else { callback(error) } }
-    else { callback(null, true) } }) }
+    else { callback(null, true) } } }
