7778f3ae34a4b50f98003986569ea26879d95217
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-17 22:36:28 -0700

    Remove unused record generation modules

--- records/annotation.js
+++
@@ -1,5 +0,0 @@
-var VERSION = require('../package.json').version
-
-module.exports = function (annotation) {
-  return {version: VERSION, annotation: annotation}
-}
--- records/form.js
+++
@@ -1,5 +0,0 @@
-var VERSION = require('../package.json').version
-
-module.exports = function (digest, form, posted) {
-  return {version: VERSION, digest: digest, form: form, posted: posted}
-}
