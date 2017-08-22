dea64fafa3310239592c0b796eccf2f6a8d22d9e
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-21 19:17:10 -0700

    Replace tv4 with ajv

--- validation/signature-pages.js
+++ validation/signature-pages.js
@@ -1,8 +1,22 @@
 var schema = require('signature-page-schema')
-var tv4 = require('tv4')
+var AJV = require('ajv')
+
+delete schema.$schema
+
+// Configure AJV to play nice with draft-04 schema.
+var ajv = new AJV({
+  meta: false,
+  extendRefs: true,
+  unknownFormats: 'ignore'
+})
+var metaSchema = require('ajv/lib/refs/json-schema-draft-04.json')
+ajv.addMetaSchema(metaSchema)
+ajv._opts.defaultMeta = metaSchema.id
+
+var validate = ajv.compile(schema)
 
 module.exports = function (argument) {
   return argument.every(function (page) {
-    return tv4.validate(page, schema)
+    return validate(page, schema)
   })
 }
