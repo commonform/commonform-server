1601a450279e0d09b7589544224a470ee912b7eb
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-13 15:51:35 -0700

    Limit release notes length

--- validation/release-notes.js
+++ validation/release-notes.js
@@ -2,10 +2,12 @@ module.exports = function (argument) {
   return (
     Array.isArray(argument) &&
     argument.length !== 0 &&
+    argument.length < 20 &&
     argument.every(function (element) {
       return (
         typeof element === 'string' &&
-        element.length !== 0
+        element.length !== 0 &&
+        element.length < 256
       )
     })
   )
